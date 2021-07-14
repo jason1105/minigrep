1b09be964a337234f4b98ecc26cd301b9f94cb30
lv-wei <13366824@qq.com>
2021-07-14 12:27:31 +0800

    12.3. Refactoring to Improve Modularity and Error Handling
    https://doc.rust-lang.org/book/ch12-03-improving-error-handling-and-modularity.html

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,31 @@
 
 // Listing 12-1: Collecting the command line arguments into a vector and printing them
 use std::env;
-use std::fs;
+use std::process;
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect(); // use instead args_os() to process non-unicode
 
-    if (args.len() < 3) {
-        println!("Usage: minigrep <query> <filename>");
-        return 
-    }
 
-    let config = parse_config(&args);
-
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("{:?}", args);
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let content = fs::read_to_string(filename).expect("Something went wrong reading the file");
-    println!("With text:\n{}", content);
+    // run(config).unwrap_or_else(|err| {
+    //     println!("Problem running: {}", err);
+    //     process::exit(1);
+    // });
 
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
 }
+
