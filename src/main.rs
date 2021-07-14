8162030ee845886720d0571387ad03baedec11b4
lv-wei <13366824@qq.com>
2021-07-14 10:28:54 +0800

    12.2. Reading a File
    https://doc.rust-lang.org/book/ch12-02-reading-a-file.html

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+// https://doc.rust-lang.org/book/ch12-01-accepting-command-line-arguments.html
+
+// Listing 12-1: Collecting the command line arguments into a vector and printing them
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect(); // use instead args_os() to process non-unicode
+
+    if (args.len() < 3) {
+        println!("Usage: minigrep <query> <filename>");
+        return 
+    }
+
+    let config = parse_config(&args);
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("{:?}", args);
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
+
+    let content = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    println!("With text:\n{}", content);
+
 }
